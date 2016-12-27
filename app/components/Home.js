18cd27a2da7658a0cfd61d33f0245f85753fa52e
ranyehushua <ranyehushua@gmail.com>
2016-12-26 19:31:10 -0700

    added search input to Home component and started adding styling (WIP)

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,24 @@
 var React = require('react');
+var Search = require('../containers/SearchContainer');
+var image = require('../img/background.jpg')
+
+var styles = {
+  body: {
+    backgroundImage: "url(" + image + ")",
+    backgroundSize: "cover",
+    backgroundPosition: 'center'
+  }
+}
 
 var Home = React.createClass({
 
   render: function() {
     return (
-      <div>Home Component</div>
+      <div style={styles.body} >
+        <div className='jumbotron text-center'>
+          <Search />
+        </div>
+      </div>
     );
   }
 
