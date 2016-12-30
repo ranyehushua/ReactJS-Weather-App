568cf8991b32d00199bc470a1d117c079dc11437
ranyehushua <ranyehushua@gmail.com>
2016-12-30 13:27:13 -0700

    Use Router Link instead of anchor tag for navbar link to root

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,12 +1,13 @@
 var React = require('react');
 var Search = require('../containers/SearchContainer');
+import { Link } from 'react-router';
 
 var Main = React.createClass({
   //lineHeight of 50px is assigned to the Search component below, in order to vertically align within 50px navbar height
   render: function() {
     return (
       <div>
-        <div className="navbar navbar-default navbar-fixed-top" style={{marginBottom: '0px'}}>
+        <div className="navbar navbar-default navbar-fixed-top" style={{marginBottom: '0px', minWidth: '230px'}}>
           <div className="container">
             <div className="navbar-header">
               <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
@@ -15,7 +16,7 @@ var Main = React.createClass({
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
               </button>
-              <a className="navbar-brand" href="/">ReactJS Weather</a>
+              <Link className="navbar-brand" to="/">ReactJS Weather</Link>
             </div>
             <div className="collapse navbar-collapse" id="navbar-collapse">
               <div className="navbar-right">
